c70a3b0edab38870ec1f3848353a1ed8281a01a7
Julian Gentges <julgentges@gmail.com>
2021-08-24 18:11:10 +0200

    Updated access control

--- Sources/PayloadValidation/PayloadValidationMiddleware.swift
+++ Sources/PayloadValidation/PayloadValidationMiddleware.swift
@@ -1,18 +1,18 @@
 import Vapor
 
-struct PayloadValidationMiddleware: Middleware {
+public struct PayloadValidationMiddleware: Middleware {
     
     let secretToken: SymmetricKey
     private let headerName: HTTPHeaders.Name
     
-    init(secretToken: String, signatureHeaderName: String) {
+    public init(secretToken: String, signatureHeaderName: String) {
         let data = Array(secretToken.utf8)
         self.secretToken = SymmetricKey(data: data)
         
         self.headerName = HTTPHeaders.Name(signatureHeaderName)
     }
     
-    func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
+    public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
         guard let requestSignature = request.headers.first(name: headerName),
               let signatureData = Data(base64Encoded: requestSignature) else {
             request.logger.debug("No signature header supplied.")
